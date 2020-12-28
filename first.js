ba56d032a59093407147852f4422165c02e18337
mohangpt <59196576+mohangpt@users.noreply.github.com>
2020-12-28 16:45:24 +0530

    Update first.js

--- first.js
+++ first.js
@@ -1,1 +1,4 @@
-(function (shop) { console.log('I am from script tag for shop ' + shop); })(Shopify.shop)
+(function (shop) { 
+   alert(`hi to ${shop}`); 
+   console.log('I am from script tag for shop ' + shop); 
+})(Shopify.shop)
